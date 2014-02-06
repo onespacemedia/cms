e0d9a2d0f1a0cd9228a52be247250416ffc3a379
Daniel Samuels <daniel.samuels1@gmail.com>
2014-02-06 16:30:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,49 +1,19 @@
+#!/usr/bin/env python
+#coding: utf-8
+
 from distutils.core import setup
-import sys, os
-sys.path.append("cms")
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-cms_dir = 'cms'
-
-for dirpath, dirnames, filenames in os.walk(cms_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
-    name='onespacemedia-cms',
-    version='1.1.2',
-    description='A collection of Django extensions that add content-management facilities to Django projects.',
-    author='Daniel Samuels',
-    author_email='daniel@onespacemedia.com',
-    url='https://github.com/onespacemedia/cms/',
-    package_dir = {
-        "cms": "cms",
-    },
-    packages = packages,
-    data_files = data_files,
-    scripts = ['cms/bin/start_cms_project.py'],
+	name = "cms",
+	author = "Daniel Samuels",
+	author_email = "daniel@onespacemedia.com",
+	version = "1.0.0",
+	license = "BSD",
+	url = "https://github.com/onespacemedia/cms",
+	download_url = "https://github.com/onespacemedia/cms",
+	description = "Onespacemedia's CMS for Django, forked from Etianen's CMS",
+	py_modules = ['cms'],
+	packages = ['cms', 'cms.apps', 'cms.apps.links', 'cms.apps.links.migrations', 'cms.apps.media', 'cms.apps.media.migrations', 'cms.apps.news', 'cms.apps.news.migrations', 'cms.apps.news.templatetags', 'cms.apps.pages', 'cms.apps.pages.migrations', 'cms.apps.pages.templatetags', 'cms.bin', 'cms.models', 'cms.project_template.project_name', 'cms.project_template.project_name.apps', 'cms.project_template.project_name.apps.site', 'cms.project_template.project_name.apps.site.migrations', 'cms.project_template.project_name.settings', 'cms.templatetags'],
+	install_requires = ['psycopg2', 'django-suit', 'django-optimizations', 'Pillow', 'django-reversion', 'django-usertools', 'django-historylinks', 'django-watson', 'South'],
+	scripts = ['cms/bin/start_cms_project.py']
 )
