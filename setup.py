f0216d291337bce4241a40bb05c79e6d43d9b9c2
Daniel Samuels <daniel.samuels1@gmail.com>
2014-02-19 09:57:50 +0000

    Fix indentation.

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ EXCLUDE_FROM_PACKAGES = ['cms.project_template',
                          'cms.bin']
 
 setup(
-	name = "cms",
+    name = "cms",
     version = "1.0.4",
     url = "https://github.com/onespacemedia/cms",
-	author = "Daniel Samuels",
-	author_email = "daniel@onespacemedia.com",
-	license = "BSD",
+    author = "Daniel Samuels",
+    author_email = "daniel@onespacemedia.com",
+    license = "BSD",
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
     scripts = ['cms/bin/start_cms_project.py'],
     zip_safe=False,
-	install_requires = ['psycopg2', 'django-suit', 'django-optimizations', 'Pillow', 'django-reversion', 'django-usertools', 'django-historylinks', 'django-watson', 'South'],
+    install_requires = ['psycopg2', 'django-suit', 'django-optimizations', 'Pillow', 'django-reversion', 'django-usertools', 'django-historylinks', 'django-watson', 'South'],
 )
