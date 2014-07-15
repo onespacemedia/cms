193babbac1374223b129fcf9c878be47c30e716c
Daniel Samuels <daniel.samuels1@gmail.com>
2014-07-15 10:02:33 +0100

    Update setup.py. Ignore egg info.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 #coding: utf-8
-import os, sys
 
 from setuptools import setup, find_packages
 
@@ -8,15 +7,16 @@ from setuptools import setup, find_packages
 EXCLUDE_FROM_PACKAGES = ['cms.bin']
 
 setup(
-    name = "onespacemedia-cms",
-    version = "1.0.4",
-    url = "https://github.com/onespacemedia/cms",
-    author = "Daniel Samuels",
-    author_email = "daniel@onespacemedia.com",
-    license = "BSD",
+    name="onespacemedia-cms",
+    version="1.0.4",
+    url="https://github.com/onespacemedia/cms",
+    author="Daniel Samuels",
+    author_email="daniel@onespacemedia.com",
+    license="BSD",
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
-    scripts = ['cms/bin/start_cms_project.py'],
+    scripts=['cms/bin/start_cms_project.py'],
     zip_safe=False,
-    install_requires = ['psycopg2', 'django-suit', 'django-optimizations', 'Pillow', 'django-reversion', 'django-usertools', 'django-historylinks', 'django-watson', 'South'],
+    description='CMS used by Onespacemedia',
+    install_requires=['psycopg2', 'django-suit', 'django-optimizations', 'Pillow', 'django-reversion', 'django-usertools', 'django-historylinks', 'django-watson', 'South'],
 )
