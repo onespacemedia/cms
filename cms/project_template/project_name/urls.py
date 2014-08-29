84fccc44be0acfd9f338f5fe102d43a00bb5001f
Daniel Samuels <daniel.samuels1@gmail.com>
2014-08-29 14:11:53 +0100

    Minor PEP8 fixes in the project template urls.py.

--- cms/project_template/project_name/urls.py
+++ cms/project_template/project_name/urls.py
@@ -13,29 +13,31 @@ from cms.views import TextTemplateView
 admin.autodiscover()
 
 
-urlpatterns = patterns("",
+urlpatterns = patterns(
+    "",
 
     # Admin URLs.
     url(r"^admin/", include(admin.site.urls)),
-    
+
     # Permalink redirection service.
     url(r"^r/(?P<content_type_id>\d+)-(?P<object_id>[^/]+)/$", "django.contrib.contenttypes.views.shortcut", name="permalink_redirect"),
-    
+
     # Google sitemap service.
     url(r"^sitemap.xml$", "django.contrib.sitemaps.views.index", {"sitemaps": registered_sitemaps}),
     url(r"^sitemap-(?P<section>.+)\.xml$", "django.contrib.sitemaps.views.sitemap", {"sitemaps": registered_sitemaps}),
-    
+
     # Basic robots.txt.
     url(r"^robots.txt$", TextTemplateView.as_view(template_name="robots.txt")),
-    
+
     # There's no favicon here!
     url(r"^favicon.ico$", generic.RedirectView.as_view()),
-    
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
-    urlpatterns += patterns("",
+    urlpatterns += patterns(
+        "",
         url("^404/$", generic.TemplateView.as_view(template_name="404.html")),
         url("^500/$", generic.TemplateView.as_view(template_name="500.html")),
     )
