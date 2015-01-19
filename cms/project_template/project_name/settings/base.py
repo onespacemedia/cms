579128fb9973233512e96846dc6335ff31e64a6b
Dan Gamble <dangamble89@gmail.com>
2015-01-19 15:02:37 +0000

    Added bcrypt to project_template

--- cms/project_template/project_name/settings/base.py
+++ cms/project_template/project_name/settings/base.py
@@ -187,6 +187,17 @@ MIDDLEWARE_CLASSES = (
     "cms.apps.pages.middleware.PageMiddleware",
 )
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
+
 ROOT_URLCONF = "{{ project_name }}.urls"
 
 WSGI_APPLICATION = "{{ project_name }}.wsgi.application"
