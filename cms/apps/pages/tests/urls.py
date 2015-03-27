9e745b0e5ac673d04d978887654627b686813d93
Daniel Samuels <daniel.samuels1@gmail.com>
2015-03-27 23:48:51 +0000

    Replace page test url views with lambdas.

--- cms/apps/pages/tests/urls.py
+++ cms/apps/pages/tests/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, url
 
 
-def view():
-    pass
-
 urlpatterns = patterns(
     "",
-    url("^$", view, name="index"),
-    url("^(?P<url_title>[^/]+)/$", view, name="detail"),
+    url("^$", lambda: None, name="index"),
+    url("^(?P<url_title>[^/]+)/$", lambda: None, name="detail"),
 )
