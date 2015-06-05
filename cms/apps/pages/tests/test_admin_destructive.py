18c93f4c70a2247bcce8a853c30038097cb9f7b2
Daniel Samuels <daniel.samuels1@gmail.com>
2015-06-05 21:01:50 +0100

    Add test for optional page admin registrations for Country and CountryGroup.

---
+++ cms/apps/pages/tests/test_admin_destructive.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.contrib import admin
+from django.test import TestCase
+
+from ..models import Country, CountryGroup, Page
+
+import sys
+
+
+class TestArticleAdminBase(TestCase):
+
+    def test_article_admin(self):
+        NEW_MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES + (
+            'cms.middleware.LocalisationMiddleware',
+        )
+
+        self.assertNotIn(Country, admin.site._registry)
+        self.assertNotIn(CountryGroup, admin.site._registry)
+
+        with self.settings(MIDDLEWARE_CLASSES=NEW_MIDDLEWARE_CLASSES):
+            module = sys.modules['cms.apps.pages.admin']
+            del sys.modules['cms.apps.pages.admin']
+
+            admin.site.unregister(Page)
+
+            from ..admin import page_admin
+            assert page_admin
+
+            self.assertIn(Country, admin.site._registry)
+            self.assertIn(CountryGroup, admin.site._registry)
+
+            sys.modules['cms.apps.pages.admin'] = module
