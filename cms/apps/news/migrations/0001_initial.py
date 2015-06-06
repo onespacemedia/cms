12d7bced3ef3e39fda214e7e383a8bae7c1e0f30
Daniel Samuels <daniel.samuels1@gmail.com>
2015-06-06 11:53:50 +0100

    Fix news migration.

--- cms/apps/news/migrations/0001_initial.py
+++ cms/apps/news/migrations/0001_initial.py
@@ -13,7 +13,7 @@ import cms.models.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pages', '0001_initial'),
+        ('pages', '0002_page_requires_authentication'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('media', '0001_initial'),
     ]
