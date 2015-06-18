ecff24511d305ad5b196276e231f55d8278ee8ef
Dan Gamble <dangamble89@gmail.com>
2015-06-18 15:17:24 +0100

    Updated Gulpfile to use Babel and split out the tasks into seperate files to keep the file clean

---
+++ cms/project_template/tasks/bower.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import {bs} from '../Gulpfile.babel';
+import config from './_config';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+export function bower() {
+  return $.bower()
+    .pipe(gulp.dest(config.bower.path));
+}
+
+export function files() {
+  gulp.src([
+    'bower_components/fastclick/lib/fastclick.js',
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/jquery-placeholder/jquery.placeholder.js',
+    'bower_components/jquery.cookie/jquery.cookie.js',
+    'bower_components/modernizr/modernizr.js',
+    'bower_components/modernizr/modernizr.js',
+  ])
+    .pipe(gulp.dest('testing/static/js/vendor/'));
+
+  gulp.src('bower_components/foundation/js/foundation/*')
+    .pipe(gulp.dest('testing/static/js/vendor/foundation/'));
+
+  gulp.src([
+    'bower_components/foundation/scss/normalize.scss',
+    'bower_components/foundation/scss/**/*',
+    '!bower_components/foundation/scss/foundation.scss',
+  ])
+    .pipe(gulp.dest('testing/static/scss/'));
+
+  gulp.src('bower_components/foundation/scss/foundation.scss')
+    .pipe($.rename({
+      basename: 'screen'
+    }))
+    .pipe(gulp.dest('testing/static/scss'));
+}
---
+++ cms/project_template/tasks/scripts.js
@@ -0,0 +1,59 @@
+/*****************************************************************************/
+/* Imports */
+/*****************************************************************************/
+// - Main
+import gulp from 'gulp';
+
+// - Gulp modules
+import gulpLoadPlugins from 'gulp-load-plugins';
+const $ = gulpLoadPlugins();
+
+// - Node modules
+import browserify from 'browserify';
+import watchify from 'watchify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+
+// - Browser Sync
+import browserSync from 'browser-sync';
+const reload = browserSync.reload;
+
+// - Project config
+import config from './_config';
+
+export function compile(watch) {
+  const bundler = watchify(browserify(config.watchify.fileIn, {debug: true}).transform(babelify));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function (err) {
+        console.error(err);
+        this.emit('end');
+      })
+      .pipe(source(config.watchify.fileOut))
+      .pipe(buffer())
+      .pipe($.sourcemaps.init({loadMaps: true}))
+      .pipe($.sourcemaps.write({includeContent: false, sourceRoot: config.watchify.folderIn}))
+      .pipe(gulp.dest(config.watchify.folderOut));
+  }
+
+  if (watch) {
+    bundler.on('update', function () {
+      // Success message
+      console.log('BUNDLED JS! ᕦ(ò_ó)ᕤ');
+
+      // Bundle the JS
+      rebundle();
+
+      // Reload browser
+      reload();
+    });
+  }
+
+  rebundle();
+}
+
+export function watch() {
+  return compile(true);
+}
