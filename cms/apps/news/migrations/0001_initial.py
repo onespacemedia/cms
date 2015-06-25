37bb8cd28b1263f6cf93d0319b6066255a53013a
Dan Gamble <dangamble89@gmail.com>
2015-06-25 15:41:05 +0100

    Deleted old news migrations

--- cms/apps/news/migrations/0001_initial.py
+++ cms/apps/news/migrations/0001_initial.py
@@ -13,9 +13,9 @@ import cms.models.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pages', '0002_page_requires_authentication'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('media', '0001_initial'),
+        ('pages', '0001_squashed_0010_page_twitter_card'),
     ]
 
     operations = [
@@ -45,7 +45,6 @@ class Migration(migrations.Migration):
                 'ordering': ('-date',),
                 'permissions': (('can_approve_articles', 'Can approve articles'),),
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Category',
@@ -69,7 +68,6 @@ class Migration(migrations.Migration):
                 'ordering': ('title',),
                 'verbose_name_plural': 'categories',
             },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='NewsFeed',
@@ -81,7 +79,6 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
-            bases=(models.Model,),
         ),
         migrations.AlterUniqueTogether(
             name='category',
@@ -90,23 +87,126 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='article',
             name='categories',
-            field=models.ManyToManyField(to='news.Category', blank=True),
-            preserve_default=True,
+            field=models.ManyToManyField(to=b'news.Category', blank=True),
         ),
         migrations.AddField(
             model_name='article',
             name='image',
             field=cms.apps.media.models.ImageRefField(related_name='+', on_delete=django.db.models.deletion.PROTECT, blank=True, to='media.File', null=True),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='article',
             name='news_feed',
             field=models.ForeignKey(default=cms.apps.news.models.get_default_news_feed, to='news.NewsFeed'),
-            preserve_default=True,
         ),
         migrations.AlterUniqueTogether(
             name='article',
             unique_together=set([('news_feed', 'date', 'url_title')]),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='slug',
+            field=models.SlugField(default='', help_text='A user friendly URL'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default='', help_text='A user friendly URL'),
+            preserve_default=False,
+        ),
+        migrations.AlterUniqueTogether(
+            name='article',
+            unique_together=set([('news_feed', 'date', 'slug')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='category',
+            unique_together=set([('slug',)]),
+        ),
+        migrations.RemoveField(
+            model_name='article',
+            name='url_title',
+        ),
+        migrations.RemoveField(
+            model_name='category',
+            name='url_title',
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='og_description',
+            field=models.TextField(help_text='Description that will appear ont he Facebook post, it is limited to 300characters but is recommended not to use anything over 200.', max_length=300, verbose_name='description', blank=True),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='og_image',
+            field=cms.apps.media.models.ImageRefField(related_name='+', on_delete=django.db.models.deletion.PROTECT, blank=True, to='media.File', help_text='The recommended image size is 1200x627 (1.91/1 ratio) this gives you a bigstand out thumbnail. Using an image smaller than 400x209 will give you a verysmall thumbnail and splits your post into 2 columns.If you have text on the image make sure it is centered as Facebook crops imagesto get the text centered so you may lose some of your image.', null=True, verbose_name='image'),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='og_title',
+            field=models.CharField(help_text='Title that will appear on the Facebook post, it is limited to 100 charactersbecause Facebook truncates the title to 88 characters.', max_length=100, verbose_name='title', blank=True),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='twitter_description',
+            field=models.TextField(help_text="Description that will appear on the Twitter card, it is limited to 200 charactersYou don't need to focus on keywords as this does'nt effect SEO so focus oncopy that compliments the tweet and title.", max_length=200, verbose_name='description', blank=True),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='twitter_image',
+            field=cms.apps.media.models.ImageRefField(related_name='+', on_delete=django.db.models.deletion.PROTECT, blank=True, to='media.File', help_text='The minimum size it needs to be is 280x150, if you want to use a larger imagemake sure the card type is set to "Large Summary"', null=True, verbose_name='image'),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='twitter_title',
+            field=models.CharField(help_text='The title that appears on the Twitter card, it is limited to 70 characters.', max_length=70, verbose_name='title', blank=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='og_description',
+            field=models.TextField(help_text='Description that will appear ont he Facebook post, it is limited to 300characters but is recommended not to use anything over 200.', max_length=300, verbose_name='description', blank=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='og_image',
+            field=cms.apps.media.models.ImageRefField(related_name='+', on_delete=django.db.models.deletion.PROTECT, blank=True, to='media.File', help_text='The recommended image size is 1200x627 (1.91/1 ratio) this gives you a bigstand out thumbnail. Using an image smaller than 400x209 will give you a verysmall thumbnail and splits your post into 2 columns.If you have text on the image make sure it is centered as Facebook crops imagesto get the text centered so you may lose some of your image.', null=True, verbose_name='image'),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='og_title',
+            field=models.CharField(help_text='Title that will appear on the Facebook post, it is limited to 100 charactersbecause Facebook truncates the title to 88 characters.', max_length=100, verbose_name='title', blank=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='twitter_description',
+            field=models.TextField(help_text="Description that will appear on the Twitter card, it is limited to 200 charactersYou don't need to focus on keywords as this does'nt effect SEO so focus oncopy that compliments the tweet and title.", max_length=200, verbose_name='description', blank=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='twitter_image',
+            field=cms.apps.media.models.ImageRefField(related_name='+', on_delete=django.db.models.deletion.PROTECT, blank=True, to='media.File', help_text='The minimum size it needs to be is 280x150, if you want to use a larger imagemake sure the card type is set to "Large Summary"', null=True, verbose_name='image'),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='twitter_title',
+            field=models.CharField(help_text='The title that appears on the Twitter card, it is limited to 70 characters.', max_length=70, verbose_name='title', blank=True),
+        ),
+        migrations.RemoveField(
+            model_name='article',
+            name='meta_keywords',
+        ),
+        migrations.RemoveField(
+            model_name='category',
+            name='meta_keywords',
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='twitter_card',
+            field=models.IntegerField(default=None, choices=[(0, 'Summary'), (1, 'Photo'), (2, 'Video'), (3, 'Product'), (4, 'App'), (5, 'Gallery'), (6, 'Large Summary')], blank=True, help_text='The type of content on the page, most of the time summary will sufficeBefore you can benefit with any of these fields make sure to go to https://dev.twitter.com/docs/cards/validation/validator and get approved', null=True, verbose_name='card'),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='twitter_card',
+            field=models.IntegerField(default=None, choices=[(0, 'Summary'), (1, 'Photo'), (2, 'Video'), (3, 'Product'), (4, 'App'), (5, 'Gallery'), (6, 'Large Summary')], blank=True, help_text='The type of content on the page, most of the time summary will sufficeBefore you can benefit with any of these fields make sure to go to https://dev.twitter.com/docs/cards/validation/validator and get approved', null=True, verbose_name='card'),
         ),
     ]
