ce761298654002bbe6638a36cd4e12481275c414
James Foley <jamesfoley@onespacemedia.com>
2015-10-14 09:37:12 +0100

    Fixed CMS HTML field tests

--- cms/tests/test_forms.py
+++ cms/tests/test_forms.py
@@ -1,3 +1,5 @@
+import json
+from django.conf import settings
 from django.test import TestCase
 
 from ..forms import CMSPasswordChangeForm, CMSAdminPasswordChangeForm, HtmlWidget, password_validation
@@ -26,18 +28,11 @@ class TestForms(TestCase):
         media = widget.get_media()
 
         self.assertDictEqual(media.__dict__, {
-            '_css': {
-                'all': ['cms/js/redactor/redactor.css']
-            },
+            '_css': {},
             '_js': [
-                '/static/cms/js/jquery.cms.js',
-                '/static/cms/js/jquery.cookie.js',
-                '/static/pages/js/jquery.cms.pages.js',
-                '/static/cms/js/redactor/redactor.js',
-                '/static/cms/js/redactor/plugins/table/table.js',
-                '/static/cms/js/redactor/plugins/imagemanager/imagemanager.js',
-                '/static/cms/js/redactor/plugins/video/video.js',
-                '/static/cms/js/redactor/plugins/filemanager/filemanager.js'
+                '/static/cms/js/ckeditor/ckeditor.js',
+                '/static/cms/js/ckeditor/adapters/jquery.js',
+                '/static/cms/js/jquery.cms.wysiwyg.js'
             ]
         })
 
@@ -48,29 +43,14 @@ class TestForms(TestCase):
 
         self.assertEqual(
             rendered,
-            '<textarea class="redactor" cols="40" name="foo" rows="10">\r\nbar<' +
-            '/textarea>'
+            '<textarea class="wysiwyg" cols="40" data-wysiwyg-settings="{}" name="foo" rows="10">\r\nbar<' +
+            '/textarea>'.format(json.dumps(getattr(settings, 'WYSIWYG_OPTIONS', {})))
         )
 
         rendered = widget.render('foo', 'bar', attrs={'id': 'foo'})
 
         self.assertIn(
-            '<textarea class="redactor" cols="40" id="foo" name="foo" rows="10">',
-            rendered,
-        )
-
-        self.assertIn(
-            '"fileUpload": "/admin/media/file/redactor/upload/file/"',
-            rendered,
-        )
-
-        self.assertIn(
-            '"imageUpload": "/admin/media/file/redactor/upload/image/"',
-            rendered,
-        )
-
-        self.assertIn(
-            '"minHeight": 300',
+            '<textarea class="wysiwyg" cols="40" data-wysiwyg-settings="{}" id="foo" name="foo" rows="10">'.format(json.dumps(getattr(settings, 'WYSIWYG_OPTIONS', {}))),
             rendered,
         )
 
