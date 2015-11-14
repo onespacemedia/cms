0764b71c85b923ac87ae91e7a26437ee7361e09c
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-14 21:54:28 +0000

    Add test for `cms.views.SearchMetaDetailMixin.get_context_data`.

--- cms/tests/test_views.py
+++ cms/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.views import generic
 
-from ..views import TextTemplateView
+from ..views import SearchMetaDetailMixin, TextTemplateView
 
 
 class TestViews(TestCase):
@@ -14,3 +15,15 @@ class TestViews(TestCase):
 
         self.assertEqual(rendered.template_name, ['templates/base.html'])
         self.assertEqual(rendered.status_code, 200)
+
+    def test_searchmetadetailmixin_get_context_data(self):
+
+        class TestClass(SearchMetaDetailMixin, generic.DetailView):
+            class Obj:
+                def get_context_data(self):
+                    return {'foo': 'bar'}
+            object = Obj()
+
+        context = TestClass().get_context_data()
+        self.assertIn('foo', context)
+        self.assertEqual(context['foo'], 'bar')
