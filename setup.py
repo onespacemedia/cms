2d1f31285ad14f956c02ad7ba9fe8584e3185a5e
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-29 14:22:46 +0000

    Move CMS folder up a level. Add setup.py and manifest file.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="onespacemedia-cms",
+    # version="2.4.2",  # Roughly..
+    url="https://github.com/onespacemedia/cms",
+    author="Daniel Samuels",
+    author_email="daniel@onespacemedia.com",
+    license="BSD",
+    packages=['cms'],
+    include_package_data=True,
+    zip_safe=False,
+    description='CMS used by Onespacemedia',
+    install_requires=[
+        'django>=1.5,<1.6'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django :: 1.5',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
