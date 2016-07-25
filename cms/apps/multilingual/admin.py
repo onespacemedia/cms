25db3f725787ace44dcc774fb573c3ae9e6d8aa5
James Foley <jamesfoley@onespacemedia.com>
2016-07-25 14:46:54 +0100

    Added translation duplication

--- cms/apps/multilingual/admin.py
+++ cms/apps/multilingual/admin.py
@@ -1,9 +1,15 @@
+from copy import deepcopy
+from functools import update_wrapper
+from random import randint
+
 from cms.apps.multilingual.models import MultilingualObject
 from cms.apps.multilingual.widgets import SmallTexarea
 from cms.apps.pages.models import Page, ContentBase
 from django import forms
 from django.contrib import admin
-from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+from django.template.response import TemplateResponse
 
 MULTILINGUAL_ADMIN_FIELDS = ['admin_name']
 MULTILINGUAL_LANGUAGE_FIELDS = ['parent', 'language', 'version', 'published']
@@ -87,6 +93,28 @@ class MultilingualTranslationAdmin(admin.ModelAdmin):
 
     list_display = ['__str__', 'language', 'version', 'published']
 
+    change_form_template = "admin/multilingual/multilingualtranslation/change_form.html"
+
+    def get_urls(self):
+        from django.conf.urls import url
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+
+        info = self.model._meta.app_label, self.model._meta.model_name
+
+        urlpatterns = [
+            url(r'^$', wrap(self.changelist_view), name='%s_%s_changelist' % info),
+            url(r'^add/$', wrap(self.add_view), name='%s_%s_add' % info),
+            url(r'^duplicate/$', wrap(self.duplicate_view), name='%s_%s_duplicate' % info),
+            url(r'^(.+)/history/$', wrap(self.history_view), name='%s_%s_history' % info),
+            url(r'^(.+)/delete/$', wrap(self.delete_view), name='%s_%s_delete' % info),
+            url(r'^(.+)/$', wrap(self.change_view), name='%s_%s_change' % info),
+        ]
+        return urlpatterns
+
     def get_form(self, request, obj=None, **kwargs):
         # Get base form
         form = super(MultilingualTranslationAdmin, self).get_form(request, obj, **kwargs)
@@ -142,3 +170,47 @@ class MultilingualTranslationAdmin(admin.ModelAdmin):
             field.label_from_instance = lambda obj: obj.admin_name
 
         return field
+
+    def duplicate_view(self, request, *args, **kwargs):
+        # Get the content object we are going to duplicate
+        content_object = get_object_or_404(self.model, pk=request.GET.get('content_object', None))
+
+        # If the user has come from the post request and its a yes...
+        if request.method == 'POST' and request.POST.get('post', 'no') == 'yes':
+
+            # Duplicate the content object
+            new_content_object = deepcopy(content_object)
+            new_content_object.pk = None  # Strip pk to save as a valid copy
+            new_content_object.published = False
+            new_content_object.version = content_object.version + 1
+            new_content_object.save()
+
+            for link in dir(content_object):
+                if link.endswith('_set') and getattr(content_object,
+                                                     link).__class__.__name__ == "RelatedManager" and link not in [
+                    'child_set', 'owner_set', 'link_to_page']:
+                    objects = getattr(content_object, link).all()
+                    for content_related_object in objects:
+
+                        # Get related field names
+                        related_fields = [field.name for field in content_related_object._meta.fields if field.related_model == self.model]
+
+                        if related_fields:
+                            new_content_related_object = deepcopy(content_related_object)
+                            new_content_related_object.pk = None
+                            setattr(new_content_related_object, related_fields[0], new_content_object)
+                            new_content_related_object.save()
+
+            messages.add_message(request, messages.INFO, 'The content object has been duplicated successfully!')
+            return redirect('admin:{}_{}_change'.format(
+                self.model._meta.app_label,
+                self.model._meta.model_name
+            ), *[new_content_object.pk])
+
+        # Template context
+        context = dict(
+            content_object=content_object
+        )
+
+        # Render template
+        return TemplateResponse(request, 'admin/multilingual/multilingualobject/language_duplicate.html', context)
