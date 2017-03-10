193088de7c5d9bf04c348fc98380973b304a06e0
Daniel Samuels <daniel.samuels1@gmail.com>
2017-03-10 10:26:31 +0000

    Fix some issues with OG tags. (#130)

--- cms/apps/pages/templatetags/pages.py
+++ cms/apps/pages/templatetags/pages.py
@@ -286,12 +286,18 @@ def get_og_image(context, image=None):
             image_obj = page.og_image
 
     if image_obj:
-        image = '{}{}'.format(
+        return '{}{}'.format(
             absolute_domain_url(context),
             image_obj.get_absolute_url()
         )
 
-    return escape(image or '')
+    if image:
+        return '{}{}'.format(
+            absolute_domain_url(context),
+            image.get_absolute_url()
+        )
+
+    return None
 
 
 @library.global_function
@@ -311,9 +317,10 @@ def get_twitter_card(context, card=None):
         homepage = request.pages.homepage
 
         # Use either the current page twitter card, or the homepage twitter card
-        if current_page and current_page.twitter_card:
+        if current_page:
             card = current_page.twitter_card
-        elif homepage and homepage.twitter_card:
+
+        if not card and homepage:
             card = homepage.twitter_card
 
     if card or card == 0:
@@ -344,7 +351,11 @@ def get_twitter_title(context, title=None):
         homepage = request.pages.homepage
 
         # Use either the current page twitter title, or the homepage twitter title
-        title = current_page.twitter_title or homepage.twitter_title
+        if current_page:
+            title = current_page.twitter_title
+
+        if not title and homepage:
+            title = homepage.twitter_title
 
         # If everything fails, fallback to OG tag title
         if not title:
@@ -378,7 +389,11 @@ def get_twitter_description(context, description=None):
         homepage = request.pages.homepage
 
         # Use either the current page twitter description, or the homepage twitter description
-        description = current_page.twitter_description or homepage.twitter_description
+        if current_page:
+            description = current_page.twitter_description
+
+        if not description and homepage:
+            description = homepage.twitter_description
 
         # If everything fails, fallback to OG tag title
         if not description:
@@ -397,11 +412,8 @@ def get_twitter_image(context, image=None):
     if not image:
         image = context.get('twitter_image')
 
-    if image:
-        image_obj = image
-
     # Check the object if we still have nothing
-    if not image and not image_obj:
+    if not image:
         obj = context.get('object')
 
         if obj:
@@ -418,21 +430,31 @@ def get_twitter_image(context, image=None):
         homepage = request.pages.homepage
 
         # Use either the current page twitter image, or the homepage twitter image
-        image = current_page.twitter_image or homepage.twitter_image
+        if current_page:
+            image = current_page.twitter_image
+
+        if not image and homepage:
+            image = homepage.twitter_image
 
         # If everything fails, fallback to OG tag title
         if not image:
-            image = get_og_image(context)
+            return get_og_image(context)
 
     # If its a file object, load the URL manually
     if image_obj:
-        image = '{}{}'.format(
+        return '{}{}'.format(
             absolute_domain_url(context),
             image_obj.get_absolute_url()
         )
 
+    if image:
+        return '{}{}'.format(
+            absolute_domain_url(context),
+            image.get_absolute_url()
+        )
+
     # Return image, or an empty string if nothing is working
-    return escape(image or '')
+    return None
 
 
 @library.global_function
