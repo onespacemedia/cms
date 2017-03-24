1b05526282e96ba57f94088b30bc020fcc218a02
Daniel Samuels <daniel@onespacemedia.com>
2017-03-24 13:07:57 +0000

    Backport 075aeaa to the 1.8.x branch.

--- cms/views.py
+++ cms/views.py
@@ -58,4 +58,7 @@ class CacheMixin(object):
         return self.cache_timeout
 
     def dispatch(self, *args, **kwargs):
+        if hasattr(self.request, 'user') and self.request.user.is_authenticated():
+            return super(CacheMixin, self).dispatch(*args, **kwargs)
+
         return cache_page(self.get_cache_timeout())(super(CacheMixin, self).dispatch)(*args, **kwargs)
