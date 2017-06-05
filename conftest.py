41cb914bdcb71ba3846f56f077d52a748f8a485e
Daniel Samuels <daniel@onespacemedia.com>
2017-06-05 15:53:16 +0100

    Change context processors to point to the new locations within Django

--- conftest.py
+++ conftest.py
@@ -79,12 +79,12 @@ def pytest_configure():
                     'translation_engine': 'django.utils.translation',
                     'context_processors': [
                         'django.contrib.auth.context_processors.auth',
-                        'django.core.context_processors.debug',
-                        'django.core.context_processors.i18n',
-                        'django.core.context_processors.media',
-                        'django.core.context_processors.static',
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.i18n',
+                        'django.template.context_processors.media',
+                        'django.template.context_processors.static',
                         'django.contrib.messages.context_processors.messages',
-                        'django.core.context_processors.request',
+                        'django.template.context_processors.request',
                         'cms.context_processors.settings',
                         'cms.apps.pages.context_processors.pages',
                     ]
@@ -99,12 +99,12 @@ def pytest_configure():
                 'OPTIONS': {
                     'context_processors': [
                         'django.contrib.auth.context_processors.auth',
-                        'django.core.context_processors.debug',
-                        'django.core.context_processors.i18n',
-                        'django.core.context_processors.media',
-                        'django.core.context_processors.static',
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.i18n',
+                        'django.template.context_processors.media',
+                        'django.template.context_processors.static',
                         'django.contrib.messages.context_processors.messages',
-                        'django.core.context_processors.request',
+                        'django.template.context_processors.request',
                         'cms.context_processors.settings',
                         'cms.apps.pages.context_processors.pages',
                     ]
