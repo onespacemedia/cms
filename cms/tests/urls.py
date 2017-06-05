92f098bc3d2fa91628f34845430d8b7473ae9b05
Daniel Samuels <daniel@onespacemedia.com>
2017-06-05 15:54:35 +0100

    Use `shortcut` as a callable rather than as a dotted path.

--- cms/tests/urls.py
+++ cms/tests/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.contrib.contenttypes.views import shortcut
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(
         r'^r/(?P<content_type_id>\d+)-(?P<object_id>[^/]+)/$',
-        'django.contrib.contenttypes.views.shortcut',
+        shortcut,
         name='permalink_redirect'
     ),
 ]
