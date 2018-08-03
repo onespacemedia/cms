3d185b62ae735e83fd815496d1588248af9806b2
Aidan Currah <aidancurrah@gmail.com>
2018-08-03 14:49:55 +0100

    Maintain file type and when saving and don't reduce jpeg quality when saving

--- cms/apps/media/forms.py
+++ cms/apps/media/forms.py
@@ -25,19 +25,23 @@ class ImageChangeForm(forms.ModelForm):
             original = File.objects.filter(file=self.cleaned_data['file']).first()
             image_data = self.cleaned_data['changed_image']
             format, imgstr = image_data.split(';base64,')
-            original_file_name = original.file.name.split('/')[-1].split('.')[0]
-            file_name = original_file_name + '.' + format
+            original_file_name = original.file.name.split('/')[-1].split('.')[0].rsplit('_', 1)[0]
+            file_name = original_file_name + '.' + format.split('/')[-1]
             data = ContentFile(base64.b64decode(imgstr), name=file_name)
 
-            # Remove alpha channel and compress to jpg.
-            image = Image.open(data)
-            image.load()  # required for png.split()
-            background = Image.new("RGB", image.size, (255, 255, 255))
-            background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
-            new_file_name = settings.MEDIA_ROOT + '/' + original.file.name.split('.')[0].split('_')[0] + '.jpg'
-            thumb_io = BytesIO()
-            background.save(thumb_io, 'JPEG', quality=80)
+            if original.file.name.split('.')[-1] in ('jpeg', 'jpg'):
+                # Remove alpha channel and compress to jpg.
+                image = Image.open(data)
+                image.load()  # required for png.split()
+                background = Image.new("RGB", image.size, (255, 255, 255))
+                background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
+                new_file_name = original_file_name + '.jpg'
+                thumb_io = BytesIO()
+                background.save(thumb_io, 'JPEG', quality=100)
 
-            self.cleaned_data['file'].save(new_file_name.split('/')[-1], content=ContentFile(thumb_io.getvalue()), save=False)
+                self.cleaned_data['file'].save(new_file_name, content=ContentFile(thumb_io.getvalue()), save=False)
+
+            else:
+                self.cleaned_data['file'].save(file_name, data)
 
         return super(ImageChangeForm, self).save(commit=commit)
