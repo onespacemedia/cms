d1545f7cb59d12153a5629977f531ade3c265f0a
Aidan Currah <aidancurrah@gmail.com>
2018-08-15 13:29:23 +0100

    Make sitemap module draggable.

--- cms/dashboard_modules.py
+++ cms/dashboard_modules.py
@@ -4,6 +4,6 @@ from jet.dashboard.modules import DashboardModule
 class SiteMap(DashboardModule):
     title = 'Sitemap'
     template = 'admin/dashboard_modules/sitemap.html'
-    draggable = False
+    draggable = True
     deletable = False
     collapsible = False
