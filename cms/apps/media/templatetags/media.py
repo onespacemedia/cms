11114d0095b9ff6bd8201d336597b8c9b1c9eb7a
Joe Hastings <joe.hastings@onespacemedia.com>
2018-11-15 17:50:58 +0000

    Add basic overriding of file picker media list modal - note this requires a couple of additions to project template to work

--- cms/apps/media/templatetags/media.py
+++ cms/apps/media/templatetags/media.py
@@ -1,7 +1,28 @@
+import json
+
+from django.template.defaultfilters import filesizeformat
+from django.utils.safestring import mark_safe
 from django_jinja import library
+from django import template
 from sorl.thumbnail import get_thumbnail
 
+from ..models import File
+
+register = template.Library()
 
 @library.global_function
 def thumbnail(path, geometry, **options):
     return get_thumbnail(path, geometry, **options)
+
+
+@library.global_function
+@register.assignment_tag()
+def osm_get_media_list():
+    file_list = [{
+        'id': file.pk,
+        'image': file.file.url,
+        'title': file.title,
+        'size': filesizeformat(file.file.size),
+    } for file in File.objects.all()]
+
+    return mark_safe(json.dumps(file_list))
