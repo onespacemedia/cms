e77b6b3c08bd80fecad2adf4747fa127caa96382
Lewis Collard <lewis.collard@onespacemedia.com>
2018-11-26 10:49:19 +0000

    Remove osm_image_upload_path templatetag as it can just as easily be done with a {% url %}.

--- cms/apps/media/templatetags/media.py
+++ cms/apps/media/templatetags/media.py
@@ -1,16 +1,7 @@
-from django.urls import reverse
 from django_jinja import library
-from django import template
 from sorl.thumbnail import get_thumbnail
 
-register = template.Library()
 
 @library.global_function
 def thumbnail(path, geometry, **options):
     return get_thumbnail(path, geometry, **options)
-
-
-@library.global_function
-@register.assignment_tag()
-def osm_image_upload_path():
-    return reverse('admin_image_upload')
