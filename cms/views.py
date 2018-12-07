91b28dad186d0a69093fa5874ff25bc5e3b25ce5
Lewis Collard <lewis.collard@onespacemedia.com>
2018-12-07 16:23:47 +0000

    Clean up more double quotes, improve some comments.

--- cms/views.py
+++ cms/views.py
@@ -1,34 +1,32 @@
-"""Views used by the CMS."""
+'''Views used by the CMS.'''
 
 from django.shortcuts import render
 from django.views import generic
 
 
 def handler500(request):
-    """Renders a pretty error page."""
-    response = render(request, "500.html", {})
+    '''Renders a pretty error page.'''
+    response = render(request, '500.html', {})
     response.status_code = 500
     return response
 
 
 class TextTemplateView(generic.TemplateView):
+    '''A template view that returns a text/plain response.'''
 
-    """A template view that returns a text/plain response."""
-
-    content_type = "text/plain; charset=utf-8"
+    content_type = 'text/plain; charset=utf-8'
 
     def render_to_response(self, context, **kwargs):
-        """Dispatches the request."""
-        kwargs.setdefault("content_type", self.content_type)
+        '''Dispatches the request.'''
+        kwargs.setdefault('content_type', self.content_type)
         return super().render_to_response(context, **kwargs)
 
 
 class SearchMetaDetailMixin:
-
-    """Generates the context for an search meta detail page."""
+    '''Generates the context for objects that inherit from SearchMetaBase.'''
 
     def get_context_data(self, **kwargs):
-        """Adds in the additional search meta context data."""
+        '''Adds in the additional search meta context data.'''
         context = super().get_context_data(**kwargs)
         defaults = self.object.get_context_data()
         defaults.update(context)
@@ -36,15 +34,12 @@ class SearchMetaDetailMixin:
 
 
 class PageDetailMixin(SearchMetaDetailMixin):
-
-    """Generates the context for a page detail view."""
+    '''Generates the context for derivatives of PageBase.'''
 
 
 class SearchMetaDetailView(SearchMetaDetailMixin, generic.DetailView):
-
-    """A simple entity detail view."""
+    '''A simple entity detail view for SearchMetaBase derivatives.'''
 
 
 class PageDetailView(PageDetailMixin, generic.DetailView):
-
-    """A simple page detail view."""
+    '''A simple detail view for PageBase derivatives.'''
