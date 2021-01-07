dba5ed40fd4058bacf43334f9e5837013517b26a
Daniel Clayton <daniel.cltn@gmail.com>
2021-01-07 16:06:04 +0000

    Move LocalisationMiddleware into pages.middleware

--- cms/apps/pages/middleware.py
+++ cms/apps/pages/middleware.py
@@ -1,13 +1,29 @@
 '''Custom middleware used by the pages application.'''
+import re
 
 from django.conf import settings
-from django.urls import resolve, is_valid_path
+from django.db.models import Q
 from django.http import Http404, HttpResponsePermanentRedirect
+from django.shortcuts import redirect
+from django.urls import resolve, is_valid_path
 from django.utils.http import escape_leading_slashes
 from django.utils.functional import cached_property
 
-from cms.apps.pages.models import Page
-from cms.apps.pages.views import PageDispatcherView
+from .models import Page, Country
+from .views import PageDispatcherView
+
+if 'cms.middleware.LocalisationMiddleware' in settings.MIDDLEWARE:
+    from django.contrib.gis.geoip2 import GeoIP2
+    from geoip2.errors import AddressNotFoundError
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 class RequestPageManager:
@@ -172,3 +188,67 @@ class PageMiddleware:
                 new_path = escape_leading_slashes(new_path)
 
                 return HttpResponsePermanentRedirect(new_path)
+
+
+class LocalisationMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.exclude_urls = [
+            re.compile(url_regex) for url_regex in getattr(settings, 'LOCALISATION_MIDDLEWARE_EXCLUDE_URLS', ())
+        ]
+
+    def __call__(self, request, geoip_path=None):
+        # Continue for media and admin
+        if any(map(lambda pattern: pattern.match(request.path_info[1:]), self.exclude_urls)):
+            return self.get_response(request)
+
+        # Set a default country object
+        request.country = None
+
+        # Check to see if we have a country in the URL
+        country_match = re.match('/([a-z]{2})/|\b', request.path)
+        if country_match:
+
+            # Try and get a country from the database
+            try:
+                request.country = Country.objects.get(
+                    code__iexact=str(country_match.group(1))
+                )
+
+                request.path = request.path.replace('/{}'.format(
+                    country_match.group(1)
+                ), '')
+                request.path_info = request.path
+
+            except Country.DoesNotExist:
+                pass
+
+        # If we don't have a country at this point, we need to do some ip
+        # checking or assumption
+        if request.country is None:
+            # Get the Geo location of the requests IP
+            geo_ip = GeoIP2(path=geoip_path)
+
+            try:
+                country_geo_ip = geo_ip.country(get_client_ip(request))
+            except AddressNotFoundError:
+                # If there's no county found for that IP, just don't look for a country
+                # and go with the default
+                country_geo_ip = {}
+
+            code = country_geo_ip.get('country_code', '')
+
+            request.country = Country.objects.filter(
+                Q(code__iexact=code) | Q(default=True)
+            ).order_by('-default').first()
+
+            if request.country:
+                return redirect('/{}{}'.format(
+                    request.country.code.lower(),
+                    request.path,
+                ))
+
+        response = self.get_response(request)
+
+        return response
--- cms/middleware.py
+++ cms/middleware.py
@@ -3,24 +3,11 @@
 import re
 
 from django.conf import settings
-from django.db.models import Q
-from django.shortcuts import redirect
 
-from cms.apps.pages.models import Country
-from cms.models import publication_manager, path_token_generator
+from .models import publication_manager, path_token_generator
 
 if 'cms.middleware.LocalisationMiddleware' in settings.MIDDLEWARE:
-    from django.contrib.gis.geoip2 import GeoIP2
-    from geoip2.errors import AddressNotFoundError
-
-
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+    from .apps.pages.middleware import LocalisationMiddleware
 
 
 class PublicationMiddleware:
@@ -63,68 +50,3 @@ class PublicationMiddleware:
 
         # Carry on as normal.
         return response
-
-
-class LocalisationMiddleware:
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-        self.exclude_urls = [
-            re.compile(url_regex) for url_regex in getattr(settings, 'LOCALISATION_MIDDLEWARE_EXCLUDE_URLS', ())
-        ]
-
-    def __call__(self, request, geoip_path=None):
-        # Continue for media and admin
-        if any(map(lambda pattern: pattern.match(request.path_info[1:]), self.exclude_urls)):
-            return self.get_response(request)
-
-        # Set a default country object
-        request.country = None
-
-        # Check to see if we have a country in the URL
-        country_match = re.match('/([a-z]{2})/|\b', request.path)
-        if country_match:
-
-            # Try and get a country from the database
-            try:
-                request.country = Country.objects.get(
-                    code__iexact=str(country_match.group(1))
-                )
-
-                request.path = request.path.replace('/{}'.format(
-                    country_match.group(1)
-                ), '')
-                request.path_info = request.path
-
-            except Country.DoesNotExist:
-                pass
-
-        # If we don't have a country at this point, we need to do some ip
-        # checking or assumption
-        if request.country is None:
-            # Get the Geo location of the requests IP
-            geo_ip = GeoIP2(path=geoip_path)
-
-            try:
-                country_geo_ip = geo_ip.country(get_client_ip(request))
-            except AddressNotFoundError:
-                # If there's no county found for that IP, just don't look for a country
-                # and go with the default
-                country_geo_ip = {}
-
-            code = country_geo_ip.get('country_code', '')
-
-            request.country = Country.objects.filter(
-                Q(code__iexact=code) | Q(default=True)
-            ).order_by('-default').first()
-
-            if request.country:
-                return redirect('/{}{}'.format(
-                    request.country.code.lower(),
-                    request.path,
-                ))
-
-        response = self.get_response(request)
-
-        return response
-
